bdbc6f46d1fe04d94ae9202bc5080b2b20cbf576
HHPresents <hiroyuki@nava21.ne.jp>
2021-09-05 10:15:29 +0900

    初版

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ValidateDispatchMemberExistence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object comObject = Activator.CreateInstance(Type.GetTypeFromProgID("Scripting.FileSystemObject"));
+
+            string validateMemberName;
+            bool result;
+
+            validateMemberName = "CreateTextFile";
+            result = DispatchMemberValidator.Exists(comObject, validateMemberName);
+            Console.WriteLine("Scripting.FileSystemObject に {0} メソッドもしくはプロパティ は {1}。", validateMemberName, result ? "存在する" : "存在しない");
+
+            validateMemberName = "CreateBinaryFile";
+            result = DispatchMemberValidator.Exists(comObject, validateMemberName);
+            Console.WriteLine("Scripting.FileSystemObject に {0} メソッドもしくはプロパティ は {1}。", validateMemberName, result ? "存在する" : "存在しない");
+
+            validateMemberName = "Drives";
+            result = DispatchMemberValidator.Exists(comObject, validateMemberName);
+            Console.WriteLine("Scripting.FileSystemObject に {0} メソッドもしくはプロパティ は {1}。", validateMemberName, result ? "存在する" : "存在しない");
+
+            Console.WriteLine("Hit Any Key.");
+            Console.ReadKey();
+        }
+    }
+
+    static class DispatchMemberValidator
+    {
+        /// <summary>
+        /// COMオブジェクトの中にメンバー(メソッド/プロパティ)は存在するか？
+        /// </summary>
+        /// <param name="comObject">検証対象のCOMオブジェクト。IDispatchが実装されている前提。</param>
+        /// <param name="memberName">確認したいメンバー名</param>
+        /// <returns>存在する場合はtrue。存在しない場合はfalse。検証時にエラーが発生した場合はComExceptionをスローする。</returns>
+        public static bool Exists(object comObject, string memberName)
+        {
+            Guid iidNull = new Guid();
+            var dispatch = (IDispatchCOM)comObject;
+            int dispid;
+            int hr = dispatch.GetIDsOfNames(ref iidNull, ref memberName, 1, LOCALE_USER_DEFAULT, out dispid);
+
+            if (hr == 0)
+            {
+                return true;
+            }
+            else if (hr == DISP_E_UNKNOWNNAME)
+            {
+                return false;
+            }
+            else
+            {
+                Marshal.ThrowExceptionForHR(hr);
+                return false; // ※ここに到達しない事は明白だが書かないとコンパイルに失敗する・・・
+            }
+        }
+        
+        private const int LOCALE_USER_DEFAULT = 1024;
+        private const int DISP_E_UNKNOWNNAME = unchecked((int)0x80020006);
+
+        /// <summary>
+        /// IDispatchのなんちゃって定義(GetIDsOfNames以外はデタラメ)
+        /// </summary>
+        [Guid("00020400-0000-0000-C000-000000000046")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        private interface IDispatchCOM
+        {
+            // 使用しないのでシグネチャはデタラメ。ただし定義順序(vtbl)は重要なので移動させたり消してはいけない。
+            [PreserveSig]
+            int GetTypeInfoCount();
+
+            // 使用しないのでシグネチャはデタラメ。ただし定義順序(vtbl)は重要なので移動させたり消してはいけない。
+            [PreserveSig]
+            int GetTypeInfo();
+
+            // 使用するのでシグネチャは正確。定義順序(vtbl)は重要なので移動させてはいけない。
+            [PreserveSig]
+            int GetIDsOfNames(ref Guid riid, [MarshalAs(UnmanagedType.LPWStr)] ref string rgszNames, int cNames, int lcid, out int rgDispId);
+
+            // 使用しないのでシグネチャはデタラメ。ただし定義順序(vtbl)は重要なので移動させたり消してはいけない。
+            [PreserveSig]
+            int Invoke();
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# ValidateDispatchMemberExistence
+ValidateDispatchMemberExistence
+====
+
+これは何？
+----
+
+COMオブジェクト(IDispatch)のメソッドおよびプロパティの存在確認をIDispatchのGetIDsOfNamesを使用して検証するサンプルプログラムです。
+
+たとえば、Microsoft Office の Excel.Application や Word.Application は、バージョンによって使用できるメソッドやプロパティが異なります。そのため、バージョン情報によって使用できるかどうかを検証する必要がありますが、直接メソッドやプロパティの存在を確認した方が確実です。
+
+技術的な説明
+----
+
+IDispatchにはGetIDsOfNameというメソッドがあります。このメソッドを使用すると、メンバー名(メソッドおよびプロパティの名前)からDISPIDを得ることができるのですが、メンバーが存在しなかった場合はDISP_E_UNKNOWNNAME(0x80020006)というエラーを返します。
+
+このような特徴を利用して、IDispatch上の動的メンバーの存在有無を検証しようという魂胆です。
+
+まず、C#にはIDispatchのインターフェース定義が無いみたいなので、GetIDsOfNameを呼び出すためだけのIDispatchのインターフェース定義をプライベートに用意します。
+
+IDispatchには４種類のメソッドが定義されているのですが、今回使用するのはGetIDsOfNameだけです。そのため、GetIDsOfName以外のメソッドのシグネチャーはデタラメでも構いません。しかし、メンバーの定義順序はvtblの順序と直結するため省略してはいけません。
+GetIDsOfNameは３番目に定義されていますので、GetIDsOfNameよりも前に宣言されているGetTypeInfoCountとGetTypeInfoの定義は必ず必要です。一方、InvokeはGetIDsOfNameよりも後に定義されているので省略しても構いません。(サンプルでは省略せずに書いてしまいました)
+
+後は、検証したいCOMオブジェクトをIDispatchに型変換してGetIDsOfNameを呼び出してみるだけです。GetIDsOfNameにPreserveSig属性をつけていればHRESULT値が直接入手できるので、DISP_E_UNKNOWNNAME(0x80020006)の発生有無を直接検証できます。
+※PreserveSig属性がついてないと .NET Framework によって COMException がスローされてしまいます。例外をキャッチしてHRESULT値を検証する方法でもよいのですが、処理が冗長になるのでお勧めしません。
